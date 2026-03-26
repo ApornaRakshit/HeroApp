c90c16440eb1bb175f234a7f32b2ff378bea677b
ApornaRakshit <rakshit.aporna@gmail.com>
2026-03-26 22:13:47 +0600

    Changed Readme

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -5,7 +5,7 @@ import Footer from '../../components/Footer/Footer';
 
 const Root = () => {
     return (
-        <div className="flex flex-col ">
+        <div className="">
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
